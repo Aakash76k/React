98aad0c766d46190da50af6ddc0157cd676e9430
Aakash <aakash76k747@gmail.com>
2026-07-11 00:43:52 +0530

    make digital clock using UseEffect

--- React-Topics/Hooks/UseEffect/src/App.jsx
+++ React-Topics/Hooks/UseEffect/src/App.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
-import FirstEffects from './FirstEffect'
+
+import DigitalClock from './DigitalClock';
 
 function App() {
   
 
   return (
     <>
-      <FirstEffects/>
+      
+      <DigitalClock/>
     </>
   )
 }
---
+++ React-Topics/Hooks/UseEffect/src/DigitalClock.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import "./DigitalClock.css";
+
+const DigitalClock = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+  return (
+    <>
+      <div className = "clock-container">
+        <h1 className = "clock-title">Digital Clock</h1>
+        <h1 className= "clock-time">{time.toLocaleTimeString()}</h1>
+        <h1 className="clock-date">{time.toDateString()}</h1>
+      </div>
+    </>
+  );
+};
+
+export default DigitalClock;
