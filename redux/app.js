40b4dfd571d5807459e50817e988f3adde502e2b
Aakash <aakash76k747@gmail.com>
2026-08-06 12:22:29 +0530

    Update Redux in React

---
+++ redux/app.js
@@ -0,0 +1,12 @@
+const {createStore} = require("redux")
+
+const initialState = {
+    accountHolder : "Subhash",
+    balance : 2000
+}
+
+function bankReducer(state = initialState, action){
+    switch(){
+        
+    }
+}
