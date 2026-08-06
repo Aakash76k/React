697e2efad314f2468f3011c1d7cd89b630ead8c2
Aakash <aakash76k747@gmail.com>
2026-08-06 12:42:26 +0530

    Update Redux topic in React

--- redux/app.js
+++ redux/app.js
@@ -1,29 +1,49 @@
 const {createStore} = require("redux")
 
 const initialState = {
-    accountHolder : "Subhash",
-    balance : 2000
+    accountHolder: "Subhash",
+    balance: 2000
 }
 
-function bankReducer(state = initialState, action){
-    switch(action.type){
+function bankReducer(state = initialState, action) {
+    switch (action.type) {
         case "DEPOSIT":
-            return{
+            return {
                 ...state,
-                balance:state.balance + action.payload
+                balance: state.balance + action.payload
             };
 
         case "WITHDRAW":
-            if(state.balance < action.payload){
+            if (state.balance < action.payload) {
                 console.log("Low balance");
                 return state;
             }
-            return{
+            return {
                 ...state,
-                balance:state.balance - action.payload
+                balance: state.balance - action.payload
             };
-            default:
-                return state;
 
+        default:
+            return state;
     }
 }
+
+const store = createStore(bankReducer)
+
+store.subscribe(() => {
+    console.log("Store Update :", store.getState())
+})
+
+console.log("Initial State:", store.getState())
+
+console.log("Action - 500 DEPOSIT")
+store.dispatch({
+    type: 'DEPOSIT',
+    payload: 500
+})
+
+console.log("Action - 5000 WITHDRAW")
+store.dispatch({
+    type: 'WITHDRAW',
+    payload: 5000
+})
