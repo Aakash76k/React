9a4642a9c118c04de5bfb14c5fafdb73eedfbcfc
Aakash <aakash76k747@gmail.com>
2026-08-12 01:19:52 +0530

    Update edit and cancel in todolist

--- Redux-p1/src/features/todo/todoApp.jsx
+++ Redux-p1/src/features/todo/todoApp.jsx
@@ -1,18 +1,42 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { addTodo, deleteTodo } from "./todoSlice";
+import { addTodo, deleteTodo, editTodo } from "./todoSlice";
 
 const TodoApp = () => {
   const items = useSelector((state) => state.todo.items);
   const dispatch = useDispatch();
 
   const [text, setText] = useState("");
+  const [editIndex, setEditIndex] = useState(null);
 
   const handleAddTodo = () => {
     if (text.trim() !== "") {
       dispatch(addTodo(text));
       setText("");
     }
+  };
+
+  const handleEditTodo = (index) => {
+    setText(items[index]);
+    setEditIndex(index);
+  };
+
+  const handleSaveTodo = () => {
+    if (text.trim() !== "") {
+      dispatch(
+        editTodo({
+          index: editIndex,
+          text: text.trim(),
+        }),
+      );
+      setText("");
+      setEditIndex(null);
+    }
+  };
+
+  const handleCancelEdit= ()=>{
+    setText("");
+    setEditIndex(null);
   };
 
   return (
@@ -22,12 +46,21 @@ const TodoApp = () => {
         value={text}
         onChange={(e) => setText(e.target.value)}
       />
-      <button onClick={handleAddTodo}> Add </button>
+      {editIndex === null ? (
+        <button onClick={handleAddTodo}> Add </button> 
+      ) : (
+        <>
+        <button onClick={handleSaveTodo}>Save</button>
+        <button onClick={handleCancelEdit}>Cancel</button>
+        </>
+      )}
+      
       <ul>
         {items.map((item, index) => (
           <li key={index}>
             {item}
             <button onClick={() => dispatch(deleteTodo(item))}>delete</button>
+            <button onClick={() => handleEditTodo(index)}>Edit</button>
           </li>
         ))}
       </ul>
